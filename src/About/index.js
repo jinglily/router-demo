9a23c89e29847d6fc5a1bdb2d0c867fb709ffc38
jingli <351201853@qq.com>
2016-12-01 09:22:29 +0800

    i

--- src/About/index.js
+++ src/About/index.js
@@ -4,6 +4,7 @@ import RaisedButton from 'material-ui/RaisedButton';
 class About extends React.Component {
   handleSubmit(e){
     e.preventDefault();
+    this.refs.form.reset(p)
 
   }
   render () {
@@ -11,7 +12,7 @@ class About extends React.Component {
       <div className='about-wrap'>
         <div className='git-card'>
           <h3 style={{color:'#26A69A'}}>Search Git Info</h3>
-          <form className='about-form' onSubmit={this.handleSubmit.bind(this)}>
+          <form ref='form' className='about-form' onSubmit={this.handleSubmit.bind(this)}>
             <TextField ref='gitname' hintText="Hint Text" underlineFocusStyle={{color:'#26A69A'}}/>
             <RaisedButton label="Search" secondary={true} style={{marginLeft:'10px'}}/>
           </form>
